21cc91ab2fa8ec9ec94d1dec714fd5df79843eb5
Aliaksandr Pratasevich <urbankomp@gmail.com>
2023-08-31 20:40:00 +0300

    feat: add slider

--- library/pages/script.js
+++ library/pages/script.js
@@ -35,4 +35,78 @@ function closeBurgerMenu(){
     menu.classList.toggle("cross-lines");
 }
 
+
+
 // library-part3
+//slider
+
+const slider = document.getElementsByClassName("slider");
+const buttonsCircle = document.getElementsByClassName("main-about-buttons-container-dots");
+const buttonContainer = document.getElementsByClassName("main-about-buttons-container");
+const buttonArrow = document.getElementsByClassName("main-about-gallery-buttons");
+
+let amountOfVisibleElements;
+
+let sliderIndex = 1;
+getAmountOfSliderBlocks();
+addEventListenerToDots();
+addEventListenerToArrow();
+showSlider(sliderIndex);
+
+function plusSlider(n) {
+  showSlider(sliderIndex += n);
+}
+
+function currentSlider(n) {
+  showSlider(sliderIndex = n);
+}
+
+function addEventListenerToArrow(){
+    for(let i = 0; i < buttonArrow.length; i++){
+        buttonArrow[i].addEventListener("click", () => {
+            plusSlider(i);
+        })
+    }
+}
+
+function addEventListenerToDots(){
+    for(let i = 0; i < buttonContainer.length; i++){
+        buttonContainer[i].addEventListener("click", () => {
+            currentSlider(i + 1);
+        })
+    }
+}
+
+function showSlider(n) {
+    if (n > slider.length) {sliderIndex = 1}
+    if (n < 1) {sliderIndex = slider.length}
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].style.display = "none";
+    }
+    for (let i = 0; i < buttonsCircle.length; i++) {
+        buttonsCircle[i].className = buttonsCircle[i].className.replace("active", "");
+    }
+    for (let i = sliderIndex-1; i < sliderIndex - 1 + amountOfVisibleElements; i++) {
+        slider[i].style.display = "block";
+    }
+    buttonsCircle[sliderIndex-1].className += " active";
+}
+
+function getAmountOfSliderBlocks(){
+    const width= document.body.clientWidth;
+    amountOfVisibleElements = width < 1008 ? 1 : 3;
+    console.log(width)
+}
+
+//width window
+
+window.addEventListener('resize',(e) => {
+    getAmountOfSliderBlocks();
+    for(let i = 0; i < slider.length; i++){
+        if (i < amountOfVisibleElements){
+            slider[i].style.display = "block";
+        } else {
+            slider[i].style.display = "none";
+        }
+    }
+});
